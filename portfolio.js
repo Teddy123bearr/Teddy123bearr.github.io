80a778b49734a3e373ba3d31dbf972af2fc888db
Teddy123bearr <122645385+Teddy123bearr@users.noreply.github.com>
2025-03-25 19:23:32 +0430

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -2,6 +2,8 @@
 const pink = document.querySelector(".pink ");
 
 // Toggle flip on click
-pink.addEventListener("click", () => {
+pink.addEventListener("click", (e) => {
+  console.log(e.target);
+  if (e.target.closest(".button")) return;
   pink.classList.toggle("active");
 });
