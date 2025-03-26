94df0788e08bb1c1befc535544965a7b34d60c93
Teddy123bearr <122645385+Teddy123bearr@users.noreply.github.com>
2025-03-26 15:13:46 +0430

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -3,7 +3,29 @@ const pink = document.querySelector(".pink ");
 
 // Toggle flip on click
 pink.addEventListener("click", (e) => {
- 
+  console.log(e.target);
   if (e.target.closest(".button-container")) return;
   pink.classList.toggle("active");
+});
+
+//progress bar
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      } else {
+        entry.target.classList.remove("visible");
+      }
+    });
+  },
+  {
+    threshold: 0.1,
+  }
+);
+
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.querySelector(".green .article-container");
+  observer.observe(container);
 });
